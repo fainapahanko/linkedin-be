dd56ae77efc92017e173f8bd94baf47017716dcf
julio <julioperezrdlf@gmail.com>
2020-01-20 17:28:49 +0100

    julio

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = express()
 const port = process.env.PORT
 
 mongoose.connect("mongodb://localhost:27017/linkedin-db",{useNewUrlParser: true})
-  .then(db => console.log("connected to mongodb"), err => console.log("error", err))
+  .then(db => console.log("connected to mongodb"), err => console.log("Error", err))
 dotenv.config();
 
 server.use(express.json())
@@ -14,4 +14,3 @@ server.use(express.json())
 server.listen(port, () => {
     console.log("We are running on localhost", port)
 })
-
