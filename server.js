e90b9a3507d158c4b1deebcbc9b62319b127c3d8
fainapahanko <f.poganko@gmail.com>
2020-01-20 20:41:41 +0100

    cors config

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const mongoose =  require("mongoose")
 const path = require("path")
 const dotenv = require("dotenv")
 const server = express()
+const cors = require("cors")
 const port = process.env.PORT
 
 mongoose.connect("mongodb://localhost:27017/linkedin-db",{useNewUrlParser: true})
   .then(db => console.log("connected to mongodb"), err => console.log("error", err))
 dotenv.config();
 
+server.use(cors())
 server.use(express.json())
 
 server.listen(port, () => {
