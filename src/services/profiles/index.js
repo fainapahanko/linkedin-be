1c1fad6c4be76201eb13b8cf3e9fb64aa6678266
Aby Babu <abyStriveschool@gmail.com>
2020-01-20 23:15:10 +0100

    profile crud done

--- src/services/profiles/index.js
+++ src/services/profiles/index.js
@@ -1,29 +1,35 @@
 const express = require("express");
 const Profile = require("../../models/profiles");
 const profilesRouter = express.Router();
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
+const generatePDF = require("./lib/generate-pdf");
 const readProfiles = async () => {
     return await Profile.find();
 };
-profilesRouter.get("/", async (req, res)=>{
+profilesRouter.get("/", async (req, res) => {
     if (req.query.name)
-        return res.send(await Profile.find({ name: req.query.name}));
+        return res.send(await Profile.find({name: req.query.name}));
     const profiles = await Profile.find({});
     res.send(profiles);
 });
-profilesRouter.get("/:_id", async(req,res)=> {
+profilesRouter.get("/:username", async (req, res) => {
 
-    const profile = await Profile.findOne({_id: req.params._id});
+    const profile = await Profile.findOne({username: req.params.username});
     if (profile) {
 
         res.send(profile);
     } else
         res.status(404).send("Not found")
 });
-profilesRouter.post("/", async (req, res)=>{
+
+
+profilesRouter.post("/", async (req, res) => {
     let profiles = await readProfiles();
     const s = profiles.find(profile => profile.name === req.body.name);
-    if (s!==undefined) {
+    if (s !== undefined) {
         res.status(400).send("INVALID NAME");
     } else {
         try {
@@ -35,7 +41,7 @@ profilesRouter.post("/", async (req, res)=>{
         }
     }
 });
-profilesRouter.put("/:id", async (req, res)=>{
+profilesRouter.put("/:id", async (req, res) => {
 
     delete req.body._id;
 
@@ -48,13 +54,42 @@ profilesRouter.put("/:id", async (req, res)=>{
     if (profile)
         res.send(profile);
     else
-        res.status(404).send("Not found " +req.params.id);
+        res.status(404).send("Not found " + req.params.id);
 });
-profilesRouter.delete("/:_id", async (req, res)=>{
-    const result = await Profile.deleteOne({ _id: req.params._id});
+profilesRouter.delete("/:id", async (req, res) => {
+    const result = await Profile.deleteOne({_id: req.params.id});
     if (result)
         res.send(result);
     else
         res.status(404).send("not found");
+});
+const upload = multer({});
+profilesRouter.post("/:username/picture", upload.single("image"), async (req, res) => {
+    //console.log(req);
+    try {
+        const imgDest = path.join(__dirname, "../../../img/profiles/" + req.params.username + req.file.originalname);
+        const imgDestination = req.protocol + "://" + req.get("host") + "/img/profiles/" + req.params.username + req.file.originalname;
+        await fs.writeFileSync(imgDest, req.file.buffer);
+        console.log(imgDestination);
+        const exp = await Profile.findOneAndUpdate({username: req.params.username}, {image: imgDestination}, {new: true, useFindAndModify: false});
+        res.send(exp)
+    } catch (err) {
+        console.log(err);
+        res.send(err)
+    }
+});
+profilesRouter.get("/:username/CV", async (req, res) => {
+    try {
+        const profile = await Profile.findOne({username: req.params.username});
+        if (profile) {
+            await generatePDF(profile); // I'm calling a function that is returning a promise so I can await for that
+            res.setHeader("Location", "/pdf/" + req.params.username + ".pdf");
+            res.status(302).send("/pdf/" + req.params.username + ".pdf");
+        } else {
+            res.status(404).send("NOT FOUND");
+        }
+    } catch (error) {
+        console.log(error);
+    }
 });
 module.exports = profilesRouter;
