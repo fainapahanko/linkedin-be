1719dd13bc88f285f7a563ee1edbd0434ee96c44
fainapahanko <f.poganko@gmail.com>
2020-01-21 16:29:00 +0100

    config

--- src/routers/posts/index.js
+++ src/routers/posts/index.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const router = express.Router();
 const Post = require("../../models/posts");
-const { check, validationResult } = require("express-validator");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
